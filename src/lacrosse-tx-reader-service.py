301b82bb994437ccf7f0a128768526fbdb952840
Dominic Miglar <dominic.miglar@netunix.at>
2018-08-14 22:11:12 +0200

    cosmetics

--- src/lacrosse-tx-reader-service.py
+++ src/lacrosse-tx-reader-service.py
@@ -4,14 +4,12 @@ import json
 import os
 import sys
 import datetime
+import traceback
 
-print("INFO: INIT: lacrosse-tx-reader-service started ...")
 
 CONFIGURATION_FILENAME = 'config.json'
-
 API_BASE_URL_ENV_VAR_NAME = 'API_BASE_URL'
 DEVICE_TOKEN_ENV_VAR_NAME = 'DEVICE_TOKEN'
-
 API_BASE_URL_DEFAULT_VALUE = 'https://api.logreposit.com/v1/'
 
 
@@ -19,12 +17,9 @@ class JSONInputNotValidError(Exception):
     pass
 
 
-def _get_log_prefix():
-    return datetime.datetime.utcnow().isoformat() + ' -> '
-
-
-def _log(message):
-    msg = _get_log_prefix() + message
+def _log(level, message):
+    log_prefix = datetime.datetime.utcnow().isoformat()
+    msg = '{} -> {}: {}'.format(log_prefix, level, message)
     print(msg)
 
 
@@ -39,7 +34,7 @@ def _read_configuration_file_and_build_mappings():
     locations = config.get('locations')
 
     if not locations:
-        _log("WARN: No locations configured.")
+        _log(level='WARN', message='No locations configured.')
         return {}
 
     mappings = {}
@@ -50,7 +45,7 @@ def _read_configuration_file_and_build_mappings():
 
         mappings[location.get('deviceId')] = location.get('name')
 
-    _log("INFO: Parsed Location-Mappings: {}".format(mappings))
+    _log(level='INFO', message='Parsed Location-Mappings: {}'.format(mappings))
 
     return mappings
 
@@ -59,30 +54,33 @@ def _check_required_environment_variables():
     device_token = os.getenv(DEVICE_TOKEN_ENV_VAR_NAME, None)
 
     if device_token is None:
-        _log('Error: you have to specify a logreposit device-token in the env var \'{}\'!'.format(
-            DEVICE_TOKEN_ENV_VAR_NAME))
+        _log(
+            level='ERROR',
+            message='Error: you have to specify a logreposit device-token in the env var \'{}\'!'.format(
+                DEVICE_TOKEN_ENV_VAR_NAME)
+        )
         sys.exit(1)
 
 
 def _validate_json_input(json_input):
     if json_input.get('time') is None:
-        _log('ERROR: JSON Input did not have an `time` field.')
+        _log(level='ERROR', message='JSON Input did not have an `time` field.')
         raise JSONInputNotValidError()
 
     if json_input.get('id') is None:
-        _log('ERROR: JSON Input did not have an `id` field.')
+        _log(level='ERROR', message='JSON Input did not have an `id` field.')
         raise JSONInputNotValidError()
 
     if json_input.get('battery') is None:
-        _log('WARN: JSON Input did not have a `battery` field.')
+        _log(level='ERROR', message='JSON Input did not have a `battery` field.')
         raise JSONInputNotValidError()
 
     if json_input.get('newbattery') is None:
-        _log('WARN: JSON Input did not have a `newbattery` field.')
+        _log(level='ERROR', message='JSON Input did not have a `newbattery` field.')
         raise JSONInputNotValidError()
 
     if json_input.get('model') is None:
-        _log('WARN: JSON Input did hot have a `model` field.')
+        _log(level='ERROR', message='JSON Input did hot have a `model` field.')
         raise JSONInputNotValidError()
 
 
@@ -97,7 +95,7 @@ def _convert_to_reading(retrieved_line, location_mappings):
     parsed_line = json.loads(retrieved_line)
 
     if not parsed_line:
-        _log('ERROR: Could not parse line: Maybe no JSON? -> {}'.format(retrieved_line))
+        _log(level='ERROR', message='Could not parse line: Maybe no JSON? -> {}'.format(retrieved_line))
         raise JSONInputNotValidError()
 
     _validate_json_input(parsed_line)
@@ -142,28 +140,27 @@ def _convert_to_reading(retrieved_line, location_mappings):
     return reading
 
 
+def _publish_values(api_base_url, device_token, reading):
+    _log(
+        level='INFO',
+        message="Publishing values to API with base-url '{}': {}".format(api_base_url, json.dumps(reading))
+    )
+
+
 def _parse_line_and_publish_values(retrieved_line, api_base_url, device_token, mappings):
-    # TODO dom: just for debugging!
-    with open('log.txt', 'a') as logfile:
-        logfile.write(datetime.datetime.utcnow().isoformat() + ' -> ' + retrieved_line)
-
     reading = _convert_to_reading(retrieved_line=retrieved_line, location_mappings=mappings)
-
-    with open('readings.txt', 'a') as logfile:
-        logfile.write(json.dumps(reading) + '\n')
 
     location_name = reading.get('location')
 
     if not location_name:
-        _log("WARN: UNKNOWN LOCATION FOR DEVICE: {}".format(retrieved_line))
+        _log(level='WARN', message='UNKNOWN LOCATION FOR DEVICE \'{}\': {}'.format(reading.get('id'), retrieved_line))
         return
 
-    _log("INFO: TODO: Publish values for location '{}' to API with base-url '{}' for deviceId '{}': {}".format(
-        location_name, api_base_url, device_token, retrieved_line))
+    _publish_values(api_base_url=api_base_url, device_token=device_token, reading=reading)
 
 
 def main():
-    print("INFO: lacrosse-tx-reader-service started ...")
+    _log(level='INFO', message='lacrosse-tx-reader-service started ...')
 
     _check_required_environment_variables()
 
@@ -175,7 +172,7 @@ def main():
         line = sys.stdin.readline()
 
         if not line:
-            _log('ERROR: No line received!')
+            _log(level='ERROR', message='No line received!')
             break
 
         try:
@@ -183,8 +180,9 @@ def main():
                                            api_base_url=api_base_url,
                                            device_token=device_token,
                                            mappings=mappings)
-        except Exception as e:
-            print('{}ERROR: Caught exception'.format(_get_log_prefix()), e)
+        except:
+            msg = traceback.format_exc()
+            _log(level='ERROR', message='Caught exception: {}'.format(msg))
 
 
 if __name__ == '__main__':
